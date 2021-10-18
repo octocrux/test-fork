ac420feb4b86666148dfec24c1fbd8f19f40738f
octocrux <cate.petrusevich@gmail.com>
2021-10-18 19:08:25 +0300

    test two

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./style.css";
 import { App } from "./components/app/app";
 
-// test comment 123
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
--- src/quiz/level1.jsx
+++ src/quiz/level1.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 
-// comment 123
 export const burgers = ref => (
   <div className="burgers" ref={ref}>
     <div className="burger for-mr-johnson">
-      
+
     </div>
     <div className="burger for-mrs-johnson">
-    
+
     </div>
     <div className="burger for-little-peter">
-    
+
     </div>
   </div>
 );
